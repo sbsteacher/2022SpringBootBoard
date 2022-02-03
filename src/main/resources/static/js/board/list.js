7fd134bee306355f649b789b7658983125c89684
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-03 11:32:58 +0900

    보드 리스트 프론트 작업 중 (2)

--- src/main/resources/static/js/board/list.js
+++ src/main/resources/static/js/board/list.js
@@ -10,9 +10,28 @@
     const getList = () => {
         myFetch.get(`/ajax/board/${icategory}`, list => {
            console.log(list);
+           makeRecodeList(list);
+        });
+    }
+
+    const makeRecodeList = list => {
+        const tbodyElem = boardListElem.querySelector('table tbody');
+        console.log(tbodyElem);
+
+        list.forEach(item => {
+            const trElem = document.createElement('tr');
+            tbodyElem.appendChild(trElem);
+
+            // 리터럴 템플릿
+            trElem.innerHTML = `
+                <td>${item.iboard}</td>
+                <td>${item.title}</td>
+                <td>${item.username}</td>
+                <td>${item.hits}</td>
+                <td>${item.rdt}</td>
+            `;
         });
     }
     getList();
 
-})()
-
+})();
