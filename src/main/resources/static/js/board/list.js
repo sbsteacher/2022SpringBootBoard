a8415d6315c38d7af5ec32d9282a254f494a9766
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-03 13:34:12 +0900

    페이징 작업 중 t_board, insert 쿼리문

--- src/main/resources/static/js/board/list.js
+++ src/main/resources/static/js/board/list.js
@@ -1,5 +1,7 @@
 (function(){
     'use strict'
+    let currentPage = 1; //현재 페이지
+    const recordCount = 10; //레코드 수
 
     const boardListElem = document.querySelector('#board_list');
     const dataElem = document.querySelector('#data');
@@ -7,6 +9,7 @@
     const searchParams = new URL(window.location.href).searchParams;
     const icategory = searchParams.get('icategory');
 
+    //글 리스트 정보 가져오기
     const getList = () => {
         myFetch.get(`/ajax/board/${icategory}`, list => {
            console.log(list);
@@ -14,6 +17,12 @@
         });
     }
 
+    //마지막 페이지 값
+    const getMaxPageVal = () => {
+
+    }
+
+    //레코드 만들기
     const makeRecodeList = list => {
         const tbodyElem = boardListElem.querySelector('table tbody');
         console.log(tbodyElem);
