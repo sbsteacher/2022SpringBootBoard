2b65323c75cc106b023f21eb19abd4a9ca5c71fb
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-04 13:29:36 +0900

    댓글 insert 백엔드 작업 완료

--- src/main/java/com/koreait/springbootboard/ResultVo.java
+++ src/main/java/com/koreait/springbootboard/ResultVo.java
@@ -1,10 +1,13 @@
 package com.koreait.springbootboard;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ResultVo {
     private int result;
 }
--- src/main/java/com/koreait/springbootboard/board/comment/CommentRestController.java
+++ src/main/java/com/koreait/springbootboard/board/comment/CommentRestController.java
@@ -2,6 +2,7 @@ package com.koreait.springbootboard.board.comment;
 
 import com.koreait.springbootboard.ResultVo;
 import com.koreait.springbootboard.board.comment.model.BoardCommentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ajax/comment")
 public class CommentRestController {
 
+    @Autowired private CommentService service;
+
     @PostMapping
     public ResultVo insComment(@RequestBody BoardCommentEntity entity) {
-        System.out.println(entity);
-        return null;
+        return service.insComment(entity);
     }
 }
